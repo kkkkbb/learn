cbe119ee0df9b9d0958f4879b819e93e44620c37
kkkkbb <2891996712@qq.com>
2025-01-30 22:30:28 +0800

    test

---
+++ src/oneday.java
@@ -0,0 +1,47 @@
+/**
+ * @author czq
+ * @version 1.0
+ */
+
+
+
+public class oneday {
+    public static void main(String[] args){
+        int res = 1 + 1;
+        System.out.println("结果= " + res);
+
+        System.out.println("老王 is studying java!");
+
+        System.out.println("我是一只小鸟，\n\t但我永远都飞不高.\"\\\\");
+
+        System.out.println("书名\t作者\t\t价格\t\t销量\n三国\t罗贯中\t120\t\t1000");
+
+
+        System.out.println("Homework01.java");
+
+        //jdk = jre + 开发工具
+        //jre = jvm + 核心类库
+
+
+    }
+}
+////
+//class Dog{
+//    public static void main(String[] args){
+//        int res = 1 + 1;
+//        System.out.println("结果= " + res);
+//
+//        System.out.println("老王 is studying java!");
+//    }
+//}
+class miss{
+    public static void main(String[] args){
+        /*
+        int
+        char
+        double
+        String
+        boolean
+         */
+    }
+}
