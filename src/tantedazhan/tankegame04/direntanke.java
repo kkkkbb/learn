f3811229fe32d254cb56fdf08958079a8dcdcec8
kkkkbb <2891996712@qq.com>
2025-02-12 23:32:48 +0800

    test

---
+++ src/tantedazhan/tankegame04/direntanke.java
@@ -0,0 +1,13 @@
+package tantedazhan.tankegame04;
+
+/**
+ * @author czq
+ * @version 1.0
+ */
+public class direntanke extends Tank {
+
+
+    public direntanke(int x, int y) {
+        super(x, y);
+    }
+}
